5ad4ec045e96f0a94cf457e16a415a2431c94261
Sidharth Baveja <sidharthbav@gmail.com>
2023-02-04 12:07:08 -0600

    Part 1

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source 'http://rubygems.org'
 
-ruby '3.2.0'
+ruby '3.2.0.preview3'
 
 gem 'byebug'
 gem 'rspec'
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,49 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  # Classic For Loop
+  # total = 0
+  # for i in arr do
+  #   total += i
+  # end
+  # total
+
+  # Block
+  # total = 0
+  # arr.each{|x| total += x}
+  # total
+
+  # Built in functionality
+  arr.sum()
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr.length() == 0
+    return 0
+  elsif arr.length() == 1
+    return arr[0]
+  else
+    largest = arr.sort[-1]
+    second_largest = arr.sort[-2]
+    return largest + second_largest
+  end
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  if arr.length() < 2
+    return false
+  else
+    target_values = Set[]
+    for val in arr do
+      diff = number - val
+      if target_values.include?(diff)
+        return true
+      end
+      target_values.add(val)
+    end
+
+    return false
+  end
 end
 
 # Part 2
